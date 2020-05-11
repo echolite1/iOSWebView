5ce4b3b0e889c19f44225d7ed09ffb60b3243c4a
Ernest Hunko <echolite221@gmail.com>
2020-05-11 10:37:19 +0200

    Project created.
    initial

---
+++ IOSWebViewTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://skorlet.de")!
+        webView.load(URLRequest(url: url))
+    }
+}
+
