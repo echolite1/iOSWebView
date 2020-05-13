bc07ba7770991c22757ea4dc9510c74f2db7d815
Ernest Hunko <echolite221@gmail.com>
2020-05-13 23:54:55 +0200

    Not the best idea to commit this one...
    for real?

--- IOSWebViewTutorial/ViewController.swift
+++ IOSWebViewTutorial/ViewController.swift
@@ -1,27 +1,34 @@
 import UIKit
 import WebKit
-import SafariServices
+//import SafariServices
 
 class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelegate {
-    var webView: WKWebView!
+//    var webView: WKWebView!
     
-    override func loadView() {
-        webView = WKWebView()
-        webView.scrollView.delegate = self
-        webView.navigationDelegate = self
-        view = webView
-    }
+//    override func loadView() {
+//        webView = WKWebView()
+//        webView.scrollView.delegate = self
+//        webView.navigationDelegate = self
+//        view = webView
+//    }
+    @IBOutlet var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
-        let url = URL(string: "https://skorlet.de")!
-        webView.load(URLRequest(url: url))
-        
-        self.navigationController?.view.backgroundColor = UIColor.green
+        webView.load(URLRequest(url: URL(string: "https://skorlet.de")!))
     }
-    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
-        scrollView.pinchGestureRecognizer?.isEnabled = false
-        //scrollView.panGestureRecognizer.isEnabled = false
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let url = URL(string: "https://skorlet.de")!
+//        webView.load(URLRequest(url: url))
+//
+//        self.navigationController?.view.backgroundColor = UIColor.green
+//    }
+//    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+//        scrollView.pinchGestureRecognizer?.isEnabled = false
+//        //scrollView.panGestureRecognizer.isEnabled = false
+//    }
 }
